86b4df4b9bf4310d682f4574c18a14e413abccac
胡永红 <>
2023-06-30 18:01:54 +0800

    影音

--- lib/http/apis.dart
+++ lib/http/apis.dart
@@ -1,3 +1,3 @@
 class API {
-  static const String baseUrl = 'https://api.cdfsunrise.com/';
+  static const String baseUrl = '';
 }
--- lib/pages/movie/movie_page.dart
+++ lib/pages/movie/movie_page.dart
@@ -2,7 +2,9 @@ import 'package:douban/components/nav_bar.dart';
 import 'package:flutter/material.dart';
 
 class MoviePage extends StatelessWidget {
-  const MoviePage({super.key});
+  MoviePage({super.key});
+
+  final tabs = ['电影', '电视', '读书', '连载', '音乐'];
 
   @override
   Widget build(BuildContext context) {
@@ -12,15 +14,7 @@ class MoviePage extends StatelessWidget {
           NavBar(title: '书影音', showBack: false,),
           Expanded(
             flex: 1,
-            child: Column(
-              children: [
-                TabBar(
-                  tabs: [Tab(text: '电影',), Tab(text: '电视',), Tab(text: '读书',), Tab(text: '连载',), Tab(text: '音乐',)],
-                  isScrollable: true,
-                  indicatorSize: TabBarIndicatorSize.label,
-                )
-              ],
-            ),
+            child: Container()
           )
         ],
       ),
